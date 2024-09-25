147641fa366177242bce1d3ae046ad6895f5bb24
caseycodes32 <meyerk15@unlv.nevada.edu>
2024-09-24 22:18:38 -0700

    made some changes to make flake8 happy

--- src/counter.py
+++ src/counter.py
@@ -6,12 +6,13 @@ app = Flask(__name__)
 
 COUNTERS = {}
 
+
 # We will use the app decorator and create a route called slash counters.
 # specify the variable in route <name>
 # let Flask know that the only methods that is allowed to called
 # on this function is "POST".
 @app.route('/counters/<name>', methods=['POST'])
-def create_counter(name):
+def create_counter(name):   
     """Create a counter"""
     app.logger.info(f"Request to create counter: {name}")
     global COUNTERS
@@ -20,15 +21,15 @@ def create_counter(name):
     COUNTERS[name] = 0
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
 
+
 @app.route('/counters/<name>', methods=['PUT'])
-def update_counter(name):
+def update_counter(name):   
     """Update a counter"""
     app.logger.info(f"Request to update a counter: {name}")
     global COUNTERS
+    #if given name exists in counters, increment counter and respond OK
     if name in COUNTERS:
         COUNTERS[name] += 1
         return {name: COUNTERS[name]}, status.HTTP_200_OK
     else:
         return {"Message":f"Counter {name} not found"}, status.HTTP_404_NOT_FOUND
-
-    
